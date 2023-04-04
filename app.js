fb750b58127a81a92f2af8c35043442b8af97fb5
werdnales <werdnales@gmail.com>
2023-04-04 23:02:33 +0300

    Make every btn functinal, remove bugs, add footer

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const gridSizeRange = document.querySelector("#grid-size");
+const colorPicker = document.querySelector("#color-picker");
 const gridContainer = document.querySelector(".square-divs");
 const dimensionRangeText = document.querySelector("#range-text");
 const increaseGrid = document.querySelector(".plus");
 const decreaseGrid = document.querySelector(".minus");
+const buttons = document.querySelectorAll(".button");
 const gridButtons = [increaseGrid, decreaseGrid];
 let gridScale = gridSizeRange.value;
 let isDrawing = false;
 let hue = 0;
 let intervalId;
+let colorPicked = `black`;
+let currentMode = "color-pick";
 
-// initiate grid generation:
-makeGrid();
-
-// divs.addEventListener("mouseleave", () => (isDrawing = false)); // Stop drawing if mouse leaves the grid borders
+// initiate grid generation and active button:
+window.onload = () => {
+  makeGrid();
+  setActiveButton(currentMode);
+};
 
 // Draw only inside the grid container:
 gridContainer.addEventListener("mousedown", () => (isDrawing = true));
@@ -22,7 +27,6 @@ document.addEventListener("mouseup", () => {
 });
 
 // Grid dimension range listener:
-// gridSizeRange.addEventListener("change", changeGridDimen);
 gridSizeRange.addEventListener("mousemove", () => {
   changeGridDimen();
 });
@@ -42,6 +46,19 @@ gridButtons.forEach((button) => {
   });
 });
 
+// Color picker:
+colorPicker.addEventListener("input", (e) => {
+  colorPicked = e.target.value;
+});
+
+// Set mode to color pick if clicked on it:
+colorPicker.addEventListener("click", () => setCurrentMode("color-pick"));
+
+// Set mode when click on button:
+buttons.forEach((button) => {
+  button.addEventListener("click", () => setCurrentMode(button.id));
+});
+
 function makeGrid() {
   dimensionRangeText.textContent = `${gridScale} x ${gridScale}`;
   gridContainer.style.gridTemplateColumns = `repeat(${gridScale}, 1fr)`;
@@ -49,13 +66,24 @@ function makeGrid() {
   for (let i = 0; i < gridScale * gridScale; i++) {
     const squareDiv = document.createElement("div");
     squareDiv.classList.add("square");
-
-    squareDiv.addEventListener("mousemove", () => {
-      if (!isDrawing) return;
-      squareDiv.style.background = `hsl(${hue}, 100%, 50%)`;
-      hue++;
-    });
+    squareDiv.addEventListener("mousedown", changeColor);
+    squareDiv.addEventListener("mouseover", changeColor);
     gridContainer.appendChild(squareDiv);
+  }
+}
+
+function changeColor(e) {
+  if (e.type === "mouseover" && !isDrawing) {
+    return;
+  } else isDrawing = true;
+
+  if (currentMode === "color-pick") {
+    e.target.style.background = colorPicked;
+  } else if (currentMode === "rainbow") {
+    e.target.style.background = `hsl(${hue}, 100%, 50%)`;
+    hue++;
+  } else if (currentMode === "eraser") {
+    e.target.style.background = "#efefef";
   }
 }
 
@@ -74,5 +102,27 @@ function changeGridDimen(increase) {
   if (prevGrid != gridScale) {
     gridContainer.innerHTML = "";
     makeGrid();
+  }
+}
+
+function setCurrentMode(newMode) {
+  if (newMode === `clear-all`) {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((sq) => {
+      sq.style.background = "#efefef";
+    });
+    return;
+  }
+  setActiveButton(newMode);
+  currentMode = newMode;
+}
+
+function setActiveButton(newMode) {
+  const activateButton = document.getElementById(newMode).classList;
+  const deactivateButton = document.getElementById(currentMode).classList;
+
+  if (newMode !== "clear-all") {
+    deactivateButton.remove("active");
+    activateButton.add("active");
   }
 }
